0b9c7e974a07622018efb92eddd942fa34d8c1a8
yonhar_uv <danilinnicita@yandex.ru>
2022-11-18 16:02:45 +0300

    Follow me

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from .models import Article
 from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import redirect   
 
 def get_article(request, article_id):
     try:
@@ -11,3 +12,28 @@ def get_article(request, article_id):
 
 def archive(request):
     return render(request, 'archive.html', {"posts": Article.objects.all()})
+
+def create_post(request):
+    if not request.user.is_anonymous:
+        if request.method == "POST":
+        # обработать данные формы, если метод POST
+            form = {
+                'text': request.POST["text"], 'title': request.POST["title"]
+            }
+        # в словаре form будет храниться информация, введенная пользователем
+            if form["text"] and form["title"]:
+                if  Article.objects.filter(title = form['title']).exists():
+                        form['errors'] = u"Статья с таким именем уже есть"
+                        return render(request, 'new.html', {'form': form})
+                else:
+                    Article.objects.create(text=form["text"], title=form["title"], author=request.user)
+                    return redirect('archive')
+            else:
+        # если введенные данные некорректны
+                form['errors'] = u"Не все поля заполнены"
+                return render(request, 'new.html', {'form': form})
+        else:
+        # просто вернуть страницу с формой, если метод GET
+            return render(request, 'new.html', {})
+    else:
+        raise Http404
--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,7 @@ from articles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.archive),
-    path('article/<int:article_id>', views.get_article, name='get_article')
+    path('', views.archive, name='archive'),
+    path('article/<int:article_id>', views.get_article, name='get_article'),
+    path('article/new/', views.create_post, name='create_post')
 ]
